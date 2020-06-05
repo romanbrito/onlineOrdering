c8b9a1c9a5c058e54053787339c169440d0b4790
Roman <romanbrito1@gmail.com>
2020-06-04 21:36:45 -0500

    adding product list

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -3,15 +3,21 @@ import React from 'react'
 import {useStaticQuery, graphql} from 'gatsby'
 
 export const PureProductList = ({data}) => {
+  console.log(data)
+
   return <div>Product List Component</div>
 }
 
 export const ProductList = props => {
   const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
+    query {
+      allStrapiDish {
+        edges {
+          node {
+            id
+            name
+            price
+          }
         }
       }
     }
