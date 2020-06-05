23d0a778bdc8055dc4a223ced0c7ee6b2fca94e4
Roman <romanbrito1@gmail.com>
2020-06-04 21:57:13 -0500

    example of data

--- src/components/ProductList/__tests__/productList.test.js
+++ src/components/ProductList/__tests__/productList.test.js
@@ -3,6 +3,11 @@ import '@testing-library/jest-dom'
 import {render, screen} from '@testing-library/react'
 import ProductList from '../index'
 
+// const data = {"allStrapiDish":{"edges":[
+//   {"node":{"id":"Dish_1","name":"test","price":333}},
+//   {"node":{"id":"Dish_2","name":"test 2","price":33}}
+// ]}}
+
 test('Renders', () => {
   render(<ProductList />)
   expect(screen.getByText(/Product List Component/i)).toBeInTheDocument()
