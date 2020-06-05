b71b7a554ebf803dba5cf33abbe62627c9361ff9
Roman <romanbrito1@gmail.com>
2020-06-04 20:21:40 -0500

    initial product list

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
+// import PropTypes from 'prop-types'
+import {useStaticQuery, graphql} from 'gatsby'
 
-const ProductList = () => {
+export const PureProductList = ({data}) => {
   return <div>Product List Component</div>
 }
 
+export const ProductList = props => {
+  const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
+  return <PureProductList {...props} data={data}></PureProductList>
+}
 export default ProductList
