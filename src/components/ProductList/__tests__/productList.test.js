3963f01b85e7d57d2239ab02b4ff125d70346429
Roman <romanbrito1@gmail.com>
2020-06-07 00:15:03 -0500

    productList test

--- src/components/ProductList/__tests__/productList.test.js
+++ src/components/ProductList/__tests__/productList.test.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import {render, screen} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import {PureProductList as ProductList} from '../index'
 
-// const data = {"allStrapiDish":{"edges":[
-//   {"node":{"id":"Dish_1","name":"test","price":333}},
-//   {"node":{"id":"Dish_2","name":"test 2","price":33}}
-// ]}}
+test('renders a product list', () => {
+  const data = {
+    allStrapiDish: {
+      edges: [
+        {node: {id: 'Dish_1', name: 'test', price: 333}},
+        {node: {id: 'Dish_2', name: 'test 2', price: 33}},
+      ],
+    },
+  }
 
-test('Renders', () => {
-  render(<ProductList />)
-  expect(screen.getByText(/Product List Component/i)).toBeInTheDocument()
+  render(<ProductList data={data} />)
+  const len = document.querySelectorAll('li').length
+  expect(len).toBe(2)
+  expect(document.querySelector('a').getAttribute('href')).toBe('/product/test')
 })
