a1c66c32d5e42af1f3abe19d0351662a20be523d
Roman <romanbrito1@gmail.com>
2020-06-06 23:46:46 -0500

    adding strapi and emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {useStaticQuery, graphql} from 'gatsby'
+import {Global} from '@emotion/core'
 
 import Header from './header'
-import './layout.css'
+import layoutStyles from './layout-styles'
 
 const Layout = ({children}) => {
   const data = useStaticQuery(graphql`
@@ -25,6 +26,7 @@ const Layout = ({children}) => {
 
   return (
     <>
+      <Global styles={layoutStyles} />
       <Header siteTitle={data.site.siteMetadata.title} />
       <div
         style={{
---
+++ src/templates/ProductPage/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductPage = ({data}) => {
+  console.log(data)
+  return (
+    <>
+      <h1>data</h1>
+    </>
+  )
+}
+
+export default ProductPage
