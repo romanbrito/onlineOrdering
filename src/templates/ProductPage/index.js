dc3b58eed05a38bd9a27ece74e94e261873ec60e
Roman <romanbrito1@gmail.com>
2020-06-22 17:21:00 -0500

    some image pages

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -3,9 +3,15 @@ import {graphql} from 'gatsby'
 
 export const PureProductPage = ({data}) => {
   const product = data.strapiMcallenproduct
+  const {images, description} = data.strapiMcallenproduct.product
   return (
     <>
       <h1>{product.name}</h1>
+      <h2>{description}</h2>
+      <img
+        src={`http://localhost:1337${images[0].formats.thumbnail.url}`}
+        alt={product.name}
+      />
     </>
   )
 }
@@ -15,6 +21,22 @@ export const query = graphql`
     strapiMcallenproduct(UID: {eq: $UID}) {
       id
       name
+      product {
+        description
+        images {
+          formats {
+            medium {
+              url
+            }
+            small {
+              url
+            }
+            thumbnail {
+              url
+            }
+          }
+        }
+      }
     }
   }
 `
