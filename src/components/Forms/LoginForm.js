a0d9546062adeaec435751894e7c5ddb0ff0d0fd
Roman <romanbrito1@gmail.com>
2020-07-22 15:39:27 -0500

    adding login component

---
+++ src/components/Forms/LoginForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ValidatedLoginForm = () => {
+  return (
+    <div>
+      <h1>Validated Form Component</h1>
+    </div>
+  )
+}
+
+export default ValidatedLoginForm
