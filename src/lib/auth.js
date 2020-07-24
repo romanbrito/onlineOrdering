b041a9e25fa961dc43d583a7e7ab7d3245763414
Roman <romanbrito1@gmail.com>
2020-07-23 22:26:28 -0500

    adding register support libraries

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -37,3 +37,35 @@ export const logout = () => {
   // todo: remove console.log
   console.log('logout')
 }
+
+export const signup = async ({username, email, password}) => {
+  // todo: make sure this is needed
+  // prevent function from being ran on the server
+  if (typeof window === 'undefined') {
+    return
+  }
+  // maybe change to try catch
+  const response = await fetch(`${API_URL}/auth/local/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      // todo: add First Name, Last Name, Phone, Terms and Privacy Policy, recieve promotions
+    }),
+  })
+
+  if (!response.ok) {
+    console.error(response.statusText)
+    return
+  }
+
+  //set token response on cookie
+  const res = await response.json()
+  Cookie.set('token', res.jwt)
+
+  return res
+}
