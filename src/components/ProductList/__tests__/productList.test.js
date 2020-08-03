bdc4fff42d7d35f9c0b268195d8042373c358a6d
Roman <romanbrito1@gmail.com>
2020-08-02 20:33:52 -0500

    adding tests

--- src/components/ProductList/__tests__/productList.test.js
+++ src/components/ProductList/__tests__/productList.test.js
@@ -6,50 +6,57 @@ import {PureProductList as ProductList} from '../index'
 // todo: remove the following, used just as reference
 // import {render, fireEvent, screen} from '@testing-library/react'
 
-test('renders a product list', () => {
-  const data = {
-    allStrapiMcallenproduct: {
-      edges: [
-        {
-          node: {
-            id: 1,
-            name: 'food1',
-            product: {
-              description: 'product 1',
-              images: [
-                {
-                  formats: {
-                    thumbnail: {
-                      url: 'https://picsum.photos/id/1043/200/200.jpg',
-                    },
+const data = {
+  allStrapiMcallenproduct: {
+    edges: [
+      {
+        node: {
+          id: 1,
+          name: 'food1',
+          product: {
+            description: 'product 1',
+            category: 'starters',
+            images: [
+              {
+                formats: {
+                  thumbnail: {
+                    url: 'https://picsum.photos/id/1043/200/200.jpg',
                   },
                 },
-              ],
-            },
+              },
+            ],
           },
         },
-        {
-          node: {
-            id: 2,
-            name: 'food2',
-            product: {
-              description: 'product 2',
-              images: [
-                {
-                  formats: {
-                    thumbnail: {
-                      url: 'https://picsum.photos/id/1043/200/200.jpg',
-                    },
+      },
+      {
+        node: {
+          id: 2,
+          name: 'food2',
+          product: {
+            description: 'product 2',
+            category: 'signature_cheesesteaks',
+            images: [
+              {
+                formats: {
+                  thumbnail: {
+                    url: 'https://picsum.photos/id/1043/200/200.jpg',
                   },
                 },
-              ],
-            },
+              },
+            ],
           },
         },
-      ],
-    },
-  }
+      },
+    ],
+  },
+}
+test('renders categories', () => {
+  const {getByText} = render(<ProductList data={data} />)
+  expect(getByText('starters')).toBeInTheDocument()
+  expect(getByText('signature cheesesteaks')).toBeInTheDocument()
+})
 
+test('renders a product list', () => {
   const {container} = render(<ProductList data={data} />)
   const len = container.querySelectorAll('li').length
   expect(len).toBe(2)
